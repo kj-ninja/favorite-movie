7b67859b98461a84b23f12ec27b69cad02f72907
kj-ninja <jakubiak.krzy@gmail.com>
2020-08-27 09:13:41 +0200

    Updates

---
+++ precache-manifest.4ade72d52f5c0be9314a8b8fab6dd905.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b881b5ddd5352f02bcc679b2e1aae7fd",
+    "url": "/favorite-movie/index.html"
+  },
+  {
+    "revision": "3fb2d7d5b4b4f6ef50af",
+    "url": "/favorite-movie/static/js/2.fd7bf58b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/favorite-movie/static/js/2.fd7bf58b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bafd69cf4f64c9dcaf36",
+    "url": "/favorite-movie/static/js/main.fe943d9b.chunk.js"
+  },
+  {
+    "revision": "8c948ff6df83bdd03923",
+    "url": "/favorite-movie/static/js/runtime-main.533296a6.js"
+  }
+]);
